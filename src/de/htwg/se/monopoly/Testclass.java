5065d409574abb599d00f908332a4ab366acccbe
Tamara <Tamara@Tamara-PC>
2014-10-24 13:35:18 +0200

    bitch please

--- src/de/htwg/se/monopoly/Testclass.java
+++ src/de/htwg/se/monopoly/Testclass.java
@@ -4,7 +4,7 @@ public class Testclass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("eclipse ist besser");
+		System.out.println("gtfo");
 	}
 
 }
